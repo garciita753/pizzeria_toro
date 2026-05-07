9e1ea6beaa802b8f3eebb36a94c66e07a048c7bb
Adalid Garcia <adalidgarcia753@gmail.com>
2026-05-07 18:51:52 -0400

    basE

--- backend/config/default.py
+++ backend/config/default.py
@@ -4,8 +4,11 @@ from datetime import timedelta
 SECRET_KEY = '123447a47f563e90fe2db0f56b1b17be62378e31b7cfd3adc776c59ca4c75e2fc512c15f69bb38307d11d5d17a41a7936789'
 
 PROPAGATE_EXCEPTIONS = True
-
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:adalid123@localhost:5432/pizzeria')
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    'DATABASE_URL',
+    'postgresql://postgres:YAfcUgUAxhjovFuGwQIaTJZlkfMdloGu@postgres.railway.internal:5432/railway'
+)
+##SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:adalid123@localhost:5432/pizzeria')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SHOW_SQLALCHEMY_LOG_MESSAGES = False
 ERROR_404_HELP = False
